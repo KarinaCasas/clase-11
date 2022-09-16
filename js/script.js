1e4913da20aa8456243cbecc0892415c434239f9
KarinaC <karinacasasj@gmail.com>
2022-09-16 00:16:35 -0300

    Aprendiendo a usar while y for

---
+++ js/script.js
@@ -0,0 +1,43 @@
+let diaSemana = prompt("¿Qué día es hoy?");
+
+switch (diaSemana) {
+    case "lunes":
+
+        alert("Hoy es lunes, debes hacer abdominales");
+
+        break;
+    case "martes":
+
+        alert("Hoy tienes que trabajar piernas");
+
+        break;
+    case "miércoles":
+
+        alert("Hoy es mitad de semana, debes trabajar los antebrazos");
+
+        break;
+    case "jueves":
+
+        alert("Hoy tienes el dia libre");
+
+        break;
+    case "viernes":
+
+        alert("Hoy debes hacer cardio");
+
+        break;
+    case "sabado":
+
+        alert("Hoy debes ir a cenar en casa de tus padres");
+
+        break;
+    case "domingo":
+
+        alert("Hoy tienes el dia libre");
+
+        break;
+    default:
+
+        alert("Escribe el día de la semana en minúsculas.");
+
+}
